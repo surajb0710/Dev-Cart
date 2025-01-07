15e6eb2368d2ae17bc518f1b096002a38c16d1d1
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-01-07 19:19:15 +0530

    HomePage: BestSeller, Footer, Newsletterbox policy components added

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,21 +3,23 @@ import ShopContext from '../context/ShopContext';
 import Title from './Title';
 import ProductItem from './ProductItem';
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const products = useContext(ShopContext);
 
-  const [latestProduct, setLatestProduct] = useState([]);
-
-  //   console.log('Products', products.products);
+  const [bestSellerProducts, setbestSellerProducts] = useState([]);
 
   useEffect(() => {
-    setLatestProduct(products.products.slice(0, 10));
+    const bestSeller = products.products.filter(
+      (product) => product.bestseller
+    );
+
+    setbestSellerProducts(bestSeller.slice(0, 5));
   }, [products.products]);
 
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1="LATEST" text2="COLLECTIONS" />
+        <Title text1="BEST" text2="SELLER" />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           ullam ipsa officia earum, temporibus laudantium animi, corrupti
@@ -27,7 +29,7 @@ const LatestCollection = () => {
       </div>
       {/*Rendering Products*/}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProduct.map((product, index) => (
+        {bestSellerProducts.map((product, index) => (
           <ProductItem
             key={index}
             id={product._id}
@@ -41,4 +43,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
+import BestSeller from '../components/BestSeller';
+import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
+import NewsletterBox from '../components/NewsletterBox';
+import Policy from '../components/Policy';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <Policy />
+      <NewsletterBox />
+      <Footer />
     </div>
   );
 };
