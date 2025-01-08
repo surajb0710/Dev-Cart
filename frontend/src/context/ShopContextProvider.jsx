94f4bdfbacc735df2b62edcdbb4d927f169f67e7
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-01-08 19:55:39 +0530

    Add to cart function updated

--- frontend/src/context/ShopContextProvider.jsx
+++ frontend/src/context/ShopContextProvider.jsx
@@ -2,12 +2,52 @@ import PropTypes from 'prop-types';
 import ShopContext from './ShopContext';
 import { products } from '../assets/assets';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const ShopContextProvider = (props) => {
   const currency = '$';
   const deliveryFee = '10';
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setcartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+
+    if (!size) {
+      toast.error('Please select size');
+      return;
+    }
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setcartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          toast.error(error);
+        }
+      }
+    }
+    return totalCount;
+  };
 
   const value = {
     products,
@@ -17,6 +57,9 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    addToCart,
+    cartItems,
+    getCartCount,
   };
 
   return (
