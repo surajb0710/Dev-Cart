56db707355d860517f04a5069536671ecd9856aa
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-01-09 00:29:48 +0530

    Order Page updated

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,5 +1,52 @@
+import Title from '../components/Title';
+import ShopContext from '../context/ShopContext';
+import { useContext } from 'react';
+
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products, currency } = useContext(ShopContext);
+
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1="MY" text2="ORDERS" />
+      </div>
+      <div>
+        {products.slice(0, 4).map((product, index) => (
+          <div
+            key={index}
+            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+          >
+            <div className="flex items-start gap-6 text-sm">
+              <img className="w-16 sm:w-20" src={product.image[0]} alt="" />
+              <div>
+                <p className="sm:text-base font-medium">{product.name}</p>
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                  <p className="text-lg">
+                    {currency}
+                    {product.price}
+                  </p>
+                  <p>Quantity:1</p>
+                  <p>Size:M</p>
+                </div>
+                <p className="mt-2">
+                  Date: <span className="text-gray-400">25, July, 2024</span>
+                </p>
+              </div>
+            </div>
+            <div className="md:w-1/2 flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                <p className="text-sm md:text-base">Ready to Ship</p>
+              </div>
+              <button className="border px-4 py-2 text-sm font-medium rounded-sm">
+                Track Order
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
